f2df115fdce9f764fdda57dee523a43d9e6c95de
wisdomandwealthteam-blip <wisdomandwealthteam@gmail.com>
2026-07-23 09:09:55 -0400

    Create __init__.py

---
+++ cps_backend/__init__.py
@@ -0,0 +1,13 @@
+from .api import (
+    get_auto_rules,
+    get_housing_rules,
+    get_auto_statutes,
+    get_housing_statutes,
+)
+
+__all__ = [
+    "get_auto_rules",
+    "get_housing_rules",
+    "get_auto_statutes",
+    "get_housing_statutes",
+]
